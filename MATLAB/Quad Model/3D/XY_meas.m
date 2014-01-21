9cab989ca155ddb6b075cc5293386218202b6c43
Diogo Almeida <digalmeda@gmail.com>
2014-01-21 14:52:39 +0100

    Removed simulation (Bad Linux compability)

--- MATLAB/Quad Model/3D/XY_meas.m
+++
@@ -1,6 +0,0 @@
-function XY_meas
-global A
-
-A.X_meas = A.X+A.X_error(A.counter);     %erros is +- 1 cm
-A.Y_meas = A.Y+A.Y_error(A.counter);     %erros is +- 1 cm
-end
--- MATLAB/Quad Model/3D/phi_meas.m
+++
@@ -1,5 +0,0 @@
-function IMU_meas
-global A
-
-A.phi_meas = A.phi+A.phi_error(A.counter)     %erros is +- 2 cm
-end
