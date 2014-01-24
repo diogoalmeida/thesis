34edf2a399c8d7bfb3d93d1d642000e6c01155ea
Diogo Almeida <digalmeda@gmail.com>
2014-01-24 15:31:01 +0100

    updates simulink PID models and added biblio

--- MATLAB/Quad Model/PID_init.m
+++ MATLAB/Quad Model/PID_init.m
@@ -3,7 +3,12 @@
 %% initial orientation
 
 % q = angle2quat(pi/4,pi/4,pi/4,'XYZ'); % roll,pitch,yaw
-q = angle2quat(pi/2,pi/2,pi/2,'XYZ');
+
+roll0=0;
+pitch0=0;
+yaw0=pi/9;
+
+q = angle2quat(roll0,pitch0,yaw0,'XYZ');
 
 qo_i=q(1);
 qv1_i=q(2);
@@ -32,3 +37,12 @@ g=9.8;
 km=342.4;
 bm=5106.8;
 d=0.16;
+
+%% Sensor variance
+
+var_x=10^-2;
+var_y=10^-2;
+var_z=10^-2;
+var_roll=pi/20;
+var_pitch=pi/20;
+var_yaw=pi/20;
