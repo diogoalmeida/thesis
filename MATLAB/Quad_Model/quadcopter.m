b79d8bfa0ba2873351e973b5ce88ed95c8509e99
Diogo Almeida <digalmeda@gmail.com>
2014-01-27 16:50:21 +0100

    removed leftovers

--- MATLAB/Quad_Model/quadcopter.m
+++
@@ -1,7 +0,0 @@
-function [ p, v, orientation, w ] = quadcopter( u, p_old, v_old, orientation_old,w_old )
-%QUADCOPTER Simulates one timestep of a quadcopter, given the inputs u =
-%[u1, u2, u3, u4]
-    
-   
-end
-
